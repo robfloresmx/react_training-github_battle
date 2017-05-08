82aac6c8b0368670f607ce336f12d104641afe15
Roberto Flores <robertoflores@Robertos-MacBook-Pro.local>
2017-05-08 18:50:36 +0200

    Finished

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ var PropTypes = require('prop-types');
 var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
 var PlayerPreview = require('./PlayerPreview');
+var Loading = require('./Loading');
 
 function Profile(props) {
     var info = props.info;
@@ -87,7 +88,7 @@ class Results extends React.Component {
         var loading = this.state.loading;
 
         if(loading === true) {
-            return <p>Loading</p>
+            return <Loading />
         }
 
         if(error) {
