ae3c98c0be60bf950687453bdd48a675a8291241
Roberto Flores <robertoflores@Robertos-MacBook-Pro.local>
2017-05-08 17:01:59 +0200

    Refactored PlayerPreview componen. Finished the Battle and Results components.

---
+++ app/components/PlayerPreview.js
@@ -0,0 +1,26 @@
+/** Whenever you use JSX, you need to require React */
+var React = require('react');
+var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
+
+function PlayerPreview(props) {
+    return(
+        <div>
+            <div className='column'>
+                <img 
+                    className='avatar'
+                    src={props.avatar}
+                    alt={'Avatar for ' + props.username} />
+                <h2 className='username'>@{props.username}</h2>
+            </div>
+            {props.children}
+        </div>
+    )
+}
+
+PlayerPreview.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+}
+
+module.exports = PlayerPreview;
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,47 @@
 var React = require('react');
 var queryString = require('query-string');
+var PropTypes = require('prop-types');
 var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
 
+function Profile(props) {
+    var info = props.info;
+    return (
+        <PlayerPreview avatar={info.avatar_url}
+            username={info.login}>
+                <ul className='space-list-items'>
+                {info.name && <li>{info.name}</li>}
+                {info.location && <li>{info.location}</li>}
+                {info.company && <li>{info.company}</li>}
+                <li>Followers: {info.followers}</li>
+                <li>Following: {info.following}</li>
+                <li>Public Repos: {info.public_repos}</li>
+                {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}            
+            </ul>
+        </PlayerPreview>
+    )
+}
+
+Profile.propTypes = {
+    info: PropTypes.object.isRequired
+};
+
+function Player(props) {
+    return (
+        <div>
+            <h1 className='header'>{props.label}</h1>
+            <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+            <Profile info={props.profile} />
+        </div>
+    )
+}
+
+Player.propTypes = {
+    label: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    profile: PropTypes.object.isRequired,
+}
 
 class Results extends React.Component {
     constructor(props) {
@@ -61,7 +100,16 @@ class Results extends React.Component {
         }
         
         return(
-            <div>{JSON.stringify(this.state)}</div>
+            <div className='row'>
+                <Player
+                    label='Winner' 
+                    score={winner.score} 
+                    profile={winner.profile} />
+                <Player
+                    label='Loser' 
+                    score={loser.score} 
+                    profile={loser.profile} />      
+            </div>
         )
     }
 }
